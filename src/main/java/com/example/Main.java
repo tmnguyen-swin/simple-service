79e7bc138cc7b708e0e1e5c397858d987c3f9a7c
Tuan Nguyen <tuan.nguyen@transtech.net.au>
2014-07-17 23:21:10 +1000

    update Main.java

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -7,9 +7,6 @@ import org.glassfish.jersey.server.ResourceConfig;
 import java.io.IOException;
 import java.net.URI;
 
- * Main class.
- *
- */
 public class Main {
     // Base URI the Grizzly HTTP server will listen on
     public static final String BASE_URI = "http://localhost:8080/myapp/";
