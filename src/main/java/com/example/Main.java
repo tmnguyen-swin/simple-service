a3588030c3ede96f95436487604b0dc848f9e28d
Tuan Nguyen <tuan.nguyen@transtech.net.au>
2014-07-17 23:57:31 +1000

    added new println statement

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -20,6 +20,7 @@ public class Main {
         final HttpServer server = startServer();
         System.out.println(String.format("Jersey app started with WADL available at "
                 + "%sapplication.wadl\nHit enter to stop it...", BASE_URI));
+	System.out.println("I added a new comment here");
         System.in.read();
         server.stop();
     }
