6730674d2938e6e9ff3b82bc354e16160a15e293
tmnguyen-swin <minhtuan2712@gmail.com>
2014-07-18 00:03:22 +1000

    Update MyResourceTest.java

--- src/test/java/com/example/MyResourceTest.java
+++ src/test/java/com/example/MyResourceTest.java
@@ -23,12 +23,6 @@ public class MyResourceTest {
         // create the client
         Client c = ClientBuilder.newClient();
 
-        // uncomment the following line if you want to enable
-        // support for JSON in the client (you also have to uncomment
-        // dependency on jersey-media-json module in pom.xml and Main.startServer())
-        // --
-        // c.configuration().enable(new org.glassfish.jersey.media.json.JsonJaxbFeature());
-
         target = c.target(Main.BASE_URI);
     }
 
