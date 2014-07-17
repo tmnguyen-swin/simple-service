df58e686cf31a55b21f8fe56fe429b36de9fe2a9
Tuan Nguyen <tuan.nguyen@transtech.net.au>
2014-07-17 23:23:35 +1000

    polish Main.java

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -8,7 +8,6 @@ import java.io.IOException;
 import java.net.URI;
 
 public class Main {
-    // Base URI the Grizzly HTTP server will listen on
     public static final String BASE_URI = "http://localhost:8080/myapp/";
 
     /**
